0908c2b955782291271a233de78de23fd7c354b9
BITniki <shakarimovdima@ya.ru>
2025-02-01 16:39:09 +0300

    cms: add peer response mapper

--- cms/src/main/java/ru/bitniki/cms/controller/ResponseMapper.java
+++ cms/src/main/java/ru/bitniki/cms/controller/ResponseMapper.java
@@ -1,7 +1,9 @@
 package ru.bitniki.cms.controller;
 
 import ru.bitniki.cms.controller.model.HostResponse;
+import ru.bitniki.cms.controller.model.PeerResponse;
 import ru.bitniki.cms.domain.hosts.dto.Host;
+import ru.bitniki.cms.domain.peers.dto.Peer;
 
 public final class ResponseMapper {
     private ResponseMapper() {
@@ -11,6 +13,16 @@ public final class ResponseMapper {
         return HostResponse.builder()
                 .id(host.id())
                 .name(host.name())
+                .build();
+    }
+
+    public static PeerResponse toPeerResponse(Peer peer) {
+        return PeerResponse.builder()
+                .id(peer.id())
+                .name(peer.name())
+                .peerStatus(PeerResponse.PeerStatusEnum.valueOf(peer.peerStatus()))
+                .ownerId(peer.ownerId())
+                .host(toHostResponse(peer.host()))
                 .build();
     }
 }
--- cms/src/test/java/ru/bitniki/cms/controller/ResponseMapperTest.java
+++ cms/src/test/java/ru/bitniki/cms/controller/ResponseMapperTest.java
@@ -1,14 +1,18 @@
 package ru.bitniki.cms.controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.bitniki.cms.controller.model.HostResponse;
+import ru.bitniki.cms.controller.model.PeerResponse;
 import ru.bitniki.cms.domain.hosts.dto.Host;
+import ru.bitniki.cms.domain.peers.dto.Peer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ResponseMapperTest {
 
     @Test
+    @DisplayName("Host: check dto to response mapping")
     void toHostResponse() {
         var expectResponse = HostResponse.builder()
                 .id(123L)
@@ -17,6 +21,33 @@ class ResponseMapperTest {
 
         var actualResponse = ResponseMapper.toHostResponse(new Host(
                 123L, "lolkek", "I pee", 123, "pass"
+        ));
+
+        assertThat(actualResponse).isEqualTo(expectResponse);
+    }
+
+    @Test
+    @DisplayName("Peer: check dto to response mapping")
+    void toPeerResponse() {
+        var expectResponse = PeerResponse.builder()
+                .id(123L)
+                .name("lel")
+                .peerStatus(PeerResponse.PeerStatusEnum.PENDING)
+                .ownerId(321L)
+                .host(
+                        HostResponse.builder()
+                        .id(123L)
+                        .name("lolkek")
+                        .build()
+                )
+                .build();
+
+        var actualResponse = ResponseMapper.toPeerResponse(new Peer(
+                123L,
+                "lel",
+                "PENDING",
+                321L,
+                new Host(123L, "lolkek", "I pee", 123, "pass")
         ));
 
         assertThat(actualResponse).isEqualTo(expectResponse);
